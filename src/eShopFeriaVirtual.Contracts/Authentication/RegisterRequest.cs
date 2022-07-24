f392087244dad810341140aaca4411c168d1123d
Alexis Aravena <aravenapro98@gmail.com>
2022-07-24 19:23:53 -0400

    Contracts API auth created

---
+++ src/eShopFeriaVirtual.Contracts/Authentication/RegisterRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopFeriaVirtual.Contracts.Authentication
+{
+    public record RegisterRequest
+    (
+        string FirstName,
+        string LastName,
+        string Email,
+        string Password
+    );
+    
+}
