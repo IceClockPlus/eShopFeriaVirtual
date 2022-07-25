6ffea585040a5e233e3d43d0c89791d8cc1238fc
Alexis Aravena <aravenapro98@gmail.com>
2022-07-24 20:05:22 -0400

    authentication controller created

---
+++ src/eShopFeriaVirtual.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using eShopFeriaVirtual.Contracts.Authentication;
+
+namespace eShopFeriaVirtual.Api.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        [Route("register")]
+        [HttpPost]
+        public IActionResult Register(RegisterRequest request)
+        {
+            return Ok();
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
