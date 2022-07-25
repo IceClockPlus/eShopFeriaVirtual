ed29bd2df4aa0df5e06a97892aef22288e9fb57a
Alexis Aravena <aravenapro98@gmail.com>
2022-07-24 20:28:33 -0400

    AuthenticationService injected on AuthenticationController

--- src/eShopFeriaVirtual.Api/Program.cs
+++ src/eShopFeriaVirtual.Api/Program.cs
@@ -1,7 +1,9 @@
+using eShopFeriaVirtual.Application.Services.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
