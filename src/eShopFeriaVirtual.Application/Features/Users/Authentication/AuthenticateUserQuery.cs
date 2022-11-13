adf0a8ffc518f24fb448ed34899d8b05894705df
IceClockPlus <aravenapro98@gmail.com>
2022-11-13 18:03:54 -0300

    Created function to login in user

---
+++ src/eShopFeriaVirtual.Application/Features/Users/Authentication/AuthenticateUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopFeriaVirtual.Application.Features.Users.Authentication
+{
+    public class AuthenticateUserQuery 
+    {
+        public string Email { get; set; };
+        public string Password { get; set; }
+    }
+}
