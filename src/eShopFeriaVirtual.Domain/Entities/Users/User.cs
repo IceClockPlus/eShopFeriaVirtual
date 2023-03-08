b611615355ac627f622f0cfa339a221b53c93b0c
Alexis Aravena <aaravena@inge.cl>
2023-03-08 16:08:05 -0300

    Created user collection in database context

---
+++ src/eShopFeriaVirtual.Domain/Entities/Users/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace eShopFeriaVirtual.Domain.Entities.Users
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+    }
+}
--- src/eShopFeriaVirtual.Infrastructure/Database/IApplicationDbContext.cs
+++ src/eShopFeriaVirtual.Infrastructure/Database/IApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using eShopFeriaVirtual.Domain.Entities.Products;
+using eShopFeriaVirtual.Domain.Entities.Users;
 using MongoDB.Driver;
 
 namespace eShopFeriaVirtual.Infrastructure.Database
@@ -6,5 +7,6 @@ namespace eShopFeriaVirtual.Infrastructure.Database
     public interface IApplicationDbContext
     {
         IMongoCollection<Product> ProductCollection { get; set; }
+        IMongoCollection<User> UserCollection { get; set; }
     }
 }
