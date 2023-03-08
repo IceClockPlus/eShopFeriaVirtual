579c34081a4fccfdc40b825e9c8e3539c10c804d
Alexis Aravena <aaravena@inge.cl>
2023-03-08 16:16:32 -0300

    Progress to code to register new users

---
+++ src/eShopFeriaVirtual.Api/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using eShopFeriaVirtual.Application.Features.Users;
+using eShopFeriaVirtual.Contracts.Authentication;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopFeriaVirtual.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("register")]
+        public async Task<ActionResult> RegisterAsync(RegisterRequest request)
+        {
+            RegisterUserCommand registerUserCommand = new()
+            {
+                Email = request.Email,
+                Name = request.FirstName,
+                LastName = request.LastName,
+                Password = request.Password,
+            };
+            var commandResult = await _mediator.Send(registerUserCommand);
+            if (commandResult.Success) return Ok(commandResult.Data);
+            return UnprocessableEntity(commandResult);
+        }
+
+    }
+}
